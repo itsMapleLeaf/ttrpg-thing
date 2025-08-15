921512bd43a4b1b79e35da7d2094861b8d663655
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-15 04:07:34 -0500

    auth: don't update existing account info

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,21 @@
 import Discord from "@auth/core/providers/discord"
 import { convexAuth } from "@convex-dev/auth/server"
+import type { MutationCtx } from "./_generated/server.js"
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
 	providers: [Discord],
+	callbacks: {
+		async createOrUpdateUser(ctx: MutationCtx, args) {
+			if (args.existingUserId) {
+				// don't update their account info
+				return args.existingUserId
+			}
+
+			return await ctx.db.insert("users", {
+				name: args.profile.name as string,
+				email: args.profile.email as string,
+				image: args.profile.image as string | undefined,
+			})
+		},
+	},
 })
