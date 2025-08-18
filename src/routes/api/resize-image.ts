f06882b43aae167061ce9e935fe3200a94842ee9
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-18 04:48:14 -0500

    lru cache because http headers make me uncomfortable

--- src/routes/api/resize-image.ts
+++ src/routes/api/resize-image.ts
@@ -1,33 +1,13 @@
 import { createServerFileRoute } from "@tanstack/react-start/server"
 import { type } from "arktype"
+import { LRUCache } from "lru-cache"
 import sharp from "sharp"
-
-const ServerRouteArgs = type({
-	url: "string",
-	width: "string.integer.parse",
-})
 
 export const ServerRoute = createServerFileRoute("/api/resize-image").methods({
 	GET: async (ctx) => {
 		try {
-			const url = new URL(ctx.request.url)
-
-			const args = ServerRouteArgs.assert({
-				url: url.searchParams.get("url"),
-				width: url.searchParams.get("width"),
-			})
-
-			const imageResponse = await fetch(args.url)
-			if (!imageResponse.ok) {
-				throw new Error(`Failed to fetch image: ${imageResponse.statusText}`)
-			}
-
-			const resized = await sharp(await imageResponse.arrayBuffer())
-				.webp({ quality: 100 })
-				.resize(args.width)
-				.toBuffer()
-
-			return new Response(new Uint8Array(resized), {
+			const result = await cache.fetch(ctx.request.url)
+			return new Response(result, {
 				headers: {
 					"Content-Type": "image/webp",
 					"Cache-Control": "public, max-age=31536000, immutable",
@@ -37,5 +17,34 @@ export const ServerRoute = createServerFileRoute("/api/resize-image").methods({
 			console.error("Error resizing image:", error)
 			return new Response("", { status: 500 })
 		}
+	},
+})
+
+const ResizeImageArgs = type({
+	url: "string",
+	width: "string.integer.parse",
+})
+
+const cache = new LRUCache({
+	max: 100,
+	fetchMethod: async (input: string) => {
+		const url = new URL(input)
+
+		const args = ResizeImageArgs.assert({
+			url: url.searchParams.get("url"),
+			width: url.searchParams.get("width"),
+		})
+
+		const imageResponse = await fetch(args.url)
+		if (!imageResponse.ok) {
+			throw new Error(`Failed to fetch image: ${imageResponse.statusText}`)
+		}
+
+		const resized = await sharp(await imageResponse.arrayBuffer())
+			.webp({ quality: 100 })
+			.resize(args.width)
+			.toBuffer()
+
+		return new Uint8Array(resized)
 	},
 })
