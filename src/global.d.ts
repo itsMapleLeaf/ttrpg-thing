79070f7dfa4bc724f23f4d2c7e6c8c994daaea12
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-18 17:38:02 -0500

    helpful useActionState overload

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,14 @@
 import "react"
 declare module "react" {
 	export function createContext<T>(): React.Context<T | undefined>
+
+	export function useActionState<State, Args = void>(
+		action: (state: State | undefined, args: Args) => State | Promise<State>,
+	): [
+		state: State | undefined,
+		dispatch: (args: Args) => void,
+		isPending: boolean,
+	]
 }
 
 declare global {
