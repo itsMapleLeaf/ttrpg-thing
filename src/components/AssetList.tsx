fbbbe8519422f53eafa6174b85a5206ce2187e6b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-22 23:15:34 -0500

    ResourceCard abstraction

--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -1,14 +1,14 @@
-import { Icon } from "@iconify/react/dist/iconify.js"
 import { useMutation, useQuery } from "convex/react"
 import { api } from "../../convex/_generated/api"
 import type { Id } from "../../convex/_generated/dataModel"
-import type { ClientAsset } from "../../convex/assets.ts"
 import { useStable } from "../hooks/useStable.ts"
 import { getOptimizedImageUrl, titleifyFileName } from "../lib/helpers.ts"
 import { Loading } from "../ui/Loading.tsx"
-import { SmartImage } from "../ui/SmartImage.tsx"
-import { WithTooltip } from "../ui/Tooltip.tsx"
-import { ResourceList, useResourceListFilterContext } from "./ResourceList.tsx"
+import {
+	ResourceCard,
+	ResourceList,
+	useResourceListFilterContext,
+} from "./ResourceList.tsx"
 
 export function AssetList({ roomId }: { roomId: Id<"rooms"> }) {
 	const { searchTerm, sortOption } = useResourceListFilterContext()
@@ -22,6 +22,7 @@ export function AssetList({ roomId }: { roomId: Id<"rooms"> }) {
 	)
 
 	const createAsset = useMutation(api.assets.create)
+	const updateAsset = useMutation(api.assets.update)
 	const removeManyAssets = useMutation(api.assets.removeMany)
 
 	return assets === undefined ? (
@@ -40,87 +41,22 @@ export function AssetList({ roomId }: { roomId: Id<"rooms"> }) {
 			removeManyResources={(ids) => removeManyAssets({ ids })}
 			renderList={(items) => (
 				<div className="grid grid-cols-2 gap-2">
-					{items.map(({ resource, selected, onSelectedChange }) => (
-						<AssetCard
+					{items.map(({ resource, selected, onChangeSelected }) => (
+						<ResourceCard
 							key={resource._id}
-							asset={resource}
+							name={resource.name}
+							imageUrl={
+								resource.url
+									? getOptimizedImageUrl(resource.url, 150).href
+									: undefined
+							}
 							selected={selected}
-							onSelectedChange={onSelectedChange}
+							onChangeSelected={onChangeSelected}
+							onChangeName={(name) => updateAsset({ id: resource._id, name })}
 						/>
 					))}
 				</div>
 			)}
 		/>
-	)
-}
-
-function AssetCard({
-	asset,
-	selected,
-	onSelectedChange,
-}: {
-	asset: ClientAsset
-	selected: boolean
-	onSelectedChange: (selected: boolean) => void
-}) {
-	const updateAsset = useMutation(api.assets.update)
-	return (
-		<div className="panel bg-gray-950/40 outline-2 outline-transparent transition-colors">
-			<label className="group relative block aspect-square">
-				{asset.url ? (
-					<SmartImage
-						src={getOptimizedImageUrl(asset.url, 150).href}
-						alt={asset.name}
-						className="size-full object-cover object-top"
-					/>
-				) : (
-					<div className="flex-center h-full">
-						<Icon
-							icon="mingcute:file-unknown-line"
-							className="size-12 opacity-50"
-						/>
-					</div>
-				)}
-
-				<div
-					className="absolute right-0 bottom-0 block p-2 opacity-0 transition-opacity group-hover:opacity-100 data-selected:opacity-100"
-					data-selected={selected || undefined}
-				>
-					<div className="sr-only">Selected</div>
-					<input
-						type="checkbox"
-						className="block size-5 accent-primary-400"
-						checked={selected}
-						onChange={(event) => onSelectedChange(event.target.checked)}
-					/>
-				</div>
-
-				<div
-					className="pointer-events-none absolute inset-0 bg-primary-400/25 opacity-0 transition-opacity data-visible:opacity-100"
-					data-visible={selected || undefined}
-				/>
-			</label>
-
-			<WithTooltip content={asset.name} positionerProps={{ side: "bottom" }}>
-				<button
-					type="button"
-					className="group flex h-6.5 w-full items-center justify-center gap-1 px-2 text-xs/tight font-semibold hover:bg-gray-800"
-					onClick={() => {
-						const newName = prompt("New name?", asset.name)?.trim()
-						if (!newName) return
-
-						updateAsset({ id: asset._id, name: newName })
-					}}
-				>
-					<div className="truncate">{asset.name}</div>
-					<div className="flex h-4 w-0 justify-end transition-[width] group-hover:w-4">
-						<Icon
-							icon="mingcute:pencil-fill"
-							className="size-4 shrink-0 opacity-0 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100"
-						/>
-					</div>
-				</button>
-			</WithTooltip>
-		</div>
 	)
 }
--- src/components/ResourceList.tsx
+++ src/components/ResourceList.tsx
@@ -7,7 +7,9 @@ import { useUploadImage } from "../hooks/useUploadImage.ts"
 import { counted } from "../lib/helpers.ts"
 import type { NonEmptyArray } from "../types.ts"
 import { Button, type ButtonProps } from "../ui/Button.tsx"
+import { SmartImage } from "../ui/SmartImage.tsx"
 import { useToastContext } from "../ui/Toast.tsx"
+import { WithTooltip } from "../ui/Tooltip.tsx"
 
 type SortOption = {
 	id: AssetListOrder
@@ -125,7 +127,7 @@ export function ResourceList<ResourceType, ResourceIdType>({
 		items: {
 			resource: ResourceType
 			selected: boolean
-			onSelectedChange: (selected: boolean) => void
+			onChangeSelected: (selected: boolean) => void
 		}[],
 	) => React.ReactNode
 }) {
@@ -267,7 +269,7 @@ export function ResourceList<ResourceType, ResourceIdType>({
 						resources.map((resource) => ({
 							resource,
 							selected: selection.has(getResourceId(resource)),
-							onSelectedChange: (selected) =>
+							onChangeSelected: (selected) =>
 								setSelected(getResourceId(resource), selected),
 						})),
 					)
@@ -328,5 +330,79 @@ function FilePicker({
 				}}
 			/>
 		</>
+	)
+}
+
+export function ResourceCard({
+	name,
+	imageUrl,
+	selected,
+	onChangeSelected,
+	onChangeName,
+}: {
+	name: string
+	imageUrl: string | null | undefined
+	selected: boolean
+	onChangeSelected: (selected: boolean) => void
+	onChangeName: (name: string) => unknown
+}) {
+	return (
+		<div className="panel bg-gray-950/40 outline-2 outline-transparent transition-colors">
+			<label className="group relative block aspect-square">
+				{imageUrl ? (
+					<SmartImage
+						src={imageUrl}
+						alt=""
+						className="size-full object-cover object-top"
+					/>
+				) : (
+					<div className="flex-center h-full">
+						<Icon
+							icon="mingcute:file-unknown-line"
+							className="size-12 opacity-50"
+						/>
+					</div>
+				)}
+
+				<div
+					className="absolute right-0 bottom-0 block p-2 opacity-0 transition-opacity group-hover:opacity-100 data-selected:opacity-100"
+					data-selected={selected || undefined}
+				>
+					<div className="sr-only">Selected</div>
+					<input
+						type="checkbox"
+						className="block size-5 accent-primary-400"
+						checked={selected}
+						onChange={(event) => onChangeSelected(event.target.checked)}
+					/>
+				</div>
+
+				<div
+					className="pointer-events-none absolute inset-0 bg-primary-400/25 opacity-0 transition-opacity data-visible:opacity-100"
+					data-visible={selected || undefined}
+				/>
+			</label>
+
+			<WithTooltip content={name} positionerProps={{ side: "bottom" }}>
+				<button
+					type="button"
+					className="group flex h-6.5 w-full items-center justify-center gap-1 px-2 text-xs/tight font-semibold hover:bg-gray-800"
+					onClick={() => {
+						const newName = prompt("New name?", name)?.trim()
+						if (!newName) return
+
+						onChangeName(newName)
+					}}
+				>
+					<div className="truncate">{name}</div>
+					<div className="flex h-4 w-0 justify-end transition-[width] group-hover:w-4">
+						<Icon
+							icon="mingcute:pencil-fill"
+							className="size-4 shrink-0 opacity-0 transition-opacity group-hover:opacity-100 group-focus-visible:opacity-100"
+						/>
+					</div>
+				</button>
+			</WithTooltip>
+		</div>
 	)
 }
