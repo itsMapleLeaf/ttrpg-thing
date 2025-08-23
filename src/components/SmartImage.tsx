1a363cfe3ea1309375aeb537359ddd29015055b0
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-22 21:03:52 -0500

    SmartImage: improvements & fix attempt

--- src/components/SmartImage.tsx
+++ src/components/SmartImage.tsx
@@ -9,22 +9,30 @@ export function SmartImage(props: ComponentProps<"img">) {
 				if (!image) return
 				if (image.complete) return
 
+				const reveal = () => {
+					image.animate([{ opacity: "0" }, { opacity: "1" }], {
+						duration: 200,
+						fill: "forwards",
+					})
+				}
+
 				const controller = new AbortController()
 
 				image.style.opacity = "0"
+				image.addEventListener("load", reveal, { signal: controller.signal })
+				image.addEventListener("error", reveal, { signal: controller.signal })
 
-				image.addEventListener(
-					"load",
-					() => {
-						image.animate([{ opacity: "0" }, { opacity: "1" }], {
-							duration: 200,
-							fill: "forwards",
-						})
-					},
-					{ signal: controller.signal },
-				)
+				// the load listener may not fire (for whatever reason),
+				// so ensure the image is revealed _eventually_
+				const timeout = setTimeout(() => {
+					// TODO: see if we can check the current load state instead of just assuming it loaded
+					reveal()
+				}, 1000)
 
-				return () => controller.abort()
+				return () => {
+					controller.abort()
+					clearTimeout(timeout)
+				}
 			}}
 		/>
 	)
