39151041fb55fd71db6e0fd97a0eb9bbc547ca07
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-23 02:18:32 -0500

    fix SmartImage double flicker

--- src/ui/SmartImage.tsx
+++ src/ui/SmartImage.tsx
@@ -14,6 +14,7 @@ export function SmartImage(props: ComponentProps<"img">) {
 						duration: 200,
 						fill: "forwards",
 					})
+					clearTimeout(timeout)
 				}
 
 				const controller = new AbortController()
