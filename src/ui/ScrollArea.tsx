9e4c6de0f04fbd3b1f78cf1c65f819c2903fcce6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-31 22:01:38 -0500

    horizontal scrollarea support

--- src/ui/ScrollArea.tsx
+++ src/ui/ScrollArea.tsx
@@ -3,15 +3,21 @@ import type { ComponentProps } from "react"
 
 export function ScrollArea({
 	children,
+	orientation = "vertical",
 	...props
-}: ComponentProps<typeof BaseScrollArea.Root>) {
+}: ComponentProps<typeof BaseScrollArea.Root> & {
+	orientation?: "horizontal" | "vertical"
+}) {
 	return (
 		<BaseScrollArea.Root {...props}>
-			<BaseScrollArea.Viewport className="h-full overscroll-contain pr-2">
+			<BaseScrollArea.Viewport className="h-full overscroll-contain">
 				{children}
 			</BaseScrollArea.Viewport>
-			<BaseScrollArea.Scrollbar className="w-2 rounded-full">
-				<BaseScrollArea.Thumb className="h-[--scroll-area-thumb-height] w-full bg-gray-700/50 transition-colors active:bg-primary-900" />
+			<BaseScrollArea.Scrollbar
+				className="p-0.5 opacity-0 transition-opacity data-hovering:opacity-100 data-[orientation=horizontal]:h-2 data-[orientation=vertical]:w-2"
+				orientation={orientation}
+			>
+				<BaseScrollArea.Thumb className="rounded bg-gray-700 transition-colors active:bg-primary-900 data-[orientation=horizontal]:h-full" />
 			</BaseScrollArea.Scrollbar>
 		</BaseScrollArea.Root>
 	)
