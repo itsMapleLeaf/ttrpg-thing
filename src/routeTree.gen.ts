791bc4dd33d6cfb862e02fcee88fcc953e6fbd14
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-09-18 04:34:53 -0500

    add back image endpoint

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,13 +8,23 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
+import { createServerRootRoute } from "@tanstack/react-start/server"
+
 import { Route as rootRouteImport } from "./routes/__root"
 import { Route as IndexRouteImport } from "./routes/index"
+import { ServerRoute as ApiImagesOptimizeServerRouteImport } from "./routes/api/images.optimize"
+
+const rootServerRouteImport = createServerRootRoute()
 
 const IndexRoute = IndexRouteImport.update({
   id: "/",
   path: "/",
   getParentRoute: () => rootRouteImport,
+} as any)
+const ApiImagesOptimizeServerRoute = ApiImagesOptimizeServerRouteImport.update({
+  id: "/api/images/optimize",
+  path: "/api/images/optimize",
+  getParentRoute: () => rootServerRouteImport,
 } as any)
 
 export interface FileRoutesByFullPath {
@@ -38,6 +48,27 @@ export interface FileRouteTypes {
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
 }
+export interface FileServerRoutesByFullPath {
+  "/api/images/optimize": typeof ApiImagesOptimizeServerRoute
+}
+export interface FileServerRoutesByTo {
+  "/api/images/optimize": typeof ApiImagesOptimizeServerRoute
+}
+export interface FileServerRoutesById {
+  __root__: typeof rootServerRouteImport
+  "/api/images/optimize": typeof ApiImagesOptimizeServerRoute
+}
+export interface FileServerRouteTypes {
+  fileServerRoutesByFullPath: FileServerRoutesByFullPath
+  fullPaths: "/api/images/optimize"
+  fileServerRoutesByTo: FileServerRoutesByTo
+  to: "/api/images/optimize"
+  id: "__root__" | "/api/images/optimize"
+  fileServerRoutesById: FileServerRoutesById
+}
+export interface RootServerRouteChildren {
+  ApiImagesOptimizeServerRoute: typeof ApiImagesOptimizeServerRoute
+}
 
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
@@ -50,6 +81,17 @@ declare module "@tanstack/react-router" {
     }
   }
 }
+declare module "@tanstack/react-start/server" {
+  interface ServerFileRoutesByPath {
+    "/api/images/optimize": {
+      id: "/api/images/optimize"
+      path: "/api/images/optimize"
+      fullPath: "/api/images/optimize"
+      preLoaderRoute: typeof ApiImagesOptimizeServerRouteImport
+      parentRoute: typeof rootServerRouteImport
+    }
+  }
+}
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
@@ -57,3 +99,9 @@ const rootRouteChildren: RootRouteChildren = {
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
+const rootServerRouteChildren: RootServerRouteChildren = {
+  ApiImagesOptimizeServerRoute: ApiImagesOptimizeServerRoute,
+}
+export const serverRouteTree = rootServerRouteImport
+  ._addFileChildren(rootServerRouteChildren)
+  ._addFileTypes<FileServerRouteTypes>()
