02ea04e6d9c6dcd3fee4bb93578863df863492e8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-10-12 20:38:02 -0500

    simplify Popover props

--- src/ui/Popover.tsx
+++ src/ui/Popover.tsx
@@ -1,20 +1,33 @@
 import { Popover as BasePopover } from "@base-ui-components/react"
+import { twMerge } from "tailwind-merge"
 
 export const Popover = BasePopover.Root
 
 export const PopoverButton = BasePopover.Trigger
+export const PopoverClose = BasePopover.Close
 
 export function PopoverPanel({
 	children,
-	positionerProps,
+	className,
+	side,
+	align,
+	gap = 8,
 }: {
 	children: React.ReactNode
-	positionerProps?: BasePopover.Positioner.Props
+	className?: string
+	side?: BasePopover.Positioner.Props["side"]
+	align?: BasePopover.Positioner.Props["align"]
+	gap?: BasePopover.Positioner.Props["sideOffset"]
 }) {
 	return (
 		<BasePopover.Portal>
-			<BasePopover.Positioner sideOffset={8} {...positionerProps}>
-				<BasePopover.Popup className="base-ui-fade-rise-transition panel shadow-md">
+			<BasePopover.Positioner side={side} align={align} sideOffset={gap}>
+				<BasePopover.Popup
+					className={twMerge(
+						"base-ui-fade-rise-transition panel shadow-md",
+						className,
+					)}
+				>
 					{children}
 				</BasePopover.Popup>
 			</BasePopover.Positioner>
